5c9f84e27603ae7479623bf437561f7ecb990f4e
Adán Marchena <adan.marchena@hotmail.com>
2022-09-09 12:21:38 -0400

    Cambios realizados para entender como funcionan las rutas

---
+++ resources/views/hola_mundo.blade.php
@@ -0,0 +1,6 @@
+<div class="alert alert-primary" role="alert">
+  Hola, bienvenido!
+</div>
+
+
+<a href="{{ route('bienvenida') }}">"Volver al inicio</a>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('bienvenida');
+
+Route::get('inicio', function () {
+    return view('Hola_mundo');
+})->name('inicio');
